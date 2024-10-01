b691234d5be48720b57ab4d6f4d20fc94d2e02e0
Ingrid Julieth Rodriguez Nova <33131209+JulietaNova@users.noreply.github.com>
2024-10-01 14:36:35 -0500

    Actualización de archivos
    Actualización del proceso de web scraping y adición del archivo en formato JSON

--- webScraping.py
+++ webScraping.py
@@ -24,7 +24,7 @@ origen = "ElTiempo"
 
 # Invocar el servicio webArchive para obtener los resultados del historico
 url = 'https://web.archive.org/cdx/search/cdx'
-parametros = {'url': 'www.eltiempo.com', 'from': '20170401', 'to': '20170420' }
+parametros = {'url': 'www.eltiempo.com', 'from': '20170311', 'to': '20240918' }
 headers = {'Accept': '*/*'}
 
 rtaWebArchive = ''
@@ -42,7 +42,6 @@ except requests.exceptions.RequestException as e:
 
 lstSnapWebArchive = rtaWebArchive.strip().split('\n')
 
-# sdfgh
 snapShotsWebArchive = {}
 for snap in lstSnapWebArchive:
     snapFecha = snap.split(' ')[1]
@@ -84,7 +83,7 @@ for fecha, item in snapShotsWebArchive.items():
     enlacesEncontrados = rtaHtmlSeccionJusticia.xpath('//a')
     
     # Filtrar URLs en los links que contengan la raíz de palabra ó palabras clave
-    palabras_clave = ["asesin", "masacre", "homicidio"]#, "feminicidio"]
+    palabras_clave = ["asesin", "masacre", "homicidio", "feminicidio"]
 
     linksInteres = [
         element.get('href') 
@@ -94,40 +93,20 @@ for fecha, item in snapShotsWebArchive.items():
 
     # Referencias de las páginas 
     for link in linksInteres:
-        urlsArticulos[link[link.find('http'):]] = item
-        
-    ####################################
-    linksAsesin_ = [element.get('href') for element in enlacesEncontrados if "asesin" in element.text_content()]
-    linksMasacre = [element.get('href') for element in enlacesEncontrados if "masacre" in element.text_content()]
-    linksHomis = [element.get('href') for element in enlacesEncontrados if "homicidio" in element.text_content()]
-
-    # Referencias de las páginas 
-    for link in linksAsesin_:
-        urlsArticulosV1[link[link.find('http'):]] = item
-    
-    for link in linksMasacre:
-        urlsArticulosV1[link[link.find('http'):]] = item
-            
-    for link in linksHomis:
-        urlsArticulosV1[link[link.find('http'):]] = item
-    ####################################
+        link = link[20:]
+        link = link[link.find('http'):]
+        if link.find('www') > 0 :
+            urlsArticulos[link.replace("http:", "https:")] = item
 
 archivoControl = []
 for articulo, snap in urlsArticulos.items():
     archivoControl.append(f"Fecha: {snap}, Artículo: {articulo}")
     
 df = pd.DataFrame({'':archivoControl})
-df.to_csv('log_ejecuciones/archivoControl2017.csv', index=False)
-
-archivoControlV1 = []
-for articulo, snap in urlsArticulosV1.items():
-    archivoControlV1.append(f"Fecha: {snap}, Artículo: {articulo}")
-
-df = pd.DataFrame({'':archivoControlV1})
-df.to_csv('log_ejecuciones/archivoControl2017V1.csv', index=False)
+df.to_csv('log_ejecuciones/archivoControl2017_2024_v2.csv', index=False)
 
 df2 = pd.DataFrame({'':snapError})
-df2.to_csv('log_ejecuciones/errores2017.csv', index=False)
+df2.to_csv('log_ejecuciones/errores2017_2024_v2.csv', index=False)
 
 end_time = datetime.now()
 print('Duration Extraccion Urls: {}'.format(end_time - start_time))
@@ -140,13 +119,14 @@ options.add_argument('--blink-settings=imagesEnabled=false')
 s = Service(os.path.dirname(os.path.abspath(__file__)) + '/chromedriver.exe')
 driver = webdriver.Chrome(options= options, service = s)
 count = 0
+articuloError = []
 
 for articulo, snap in urlsArticulos.items():
     linkArticulo = 'https://web.archive.org/web/' + snap +'/'+ articulo
     try:
         # Link de la página
         driver.get(linkArticulo)
-        time.sleep(10)
+        time.sleep(5)
     except TimeoutException:
         pass
     
@@ -158,25 +138,40 @@ for articulo, snap in urlsArticulos.items():
 
     try:
         titulo = (driver.find_element(By.TAG_NAME, "h1")).text
+        if titulo == "" :
+            el_titulo = driver.find_elements(By.TAG_NAME, "h1")
+            if len(el_titulo) > 1 :
+                titulo = driver.find_elements(By.TAG_NAME, "h1")[1].text
     except NoSuchElementException:
         subtitulo = (driver.find_element(By.TAG_NAME, "h2")).text
-    
-    contenido = (driver.find_elements(By.CLASS_NAME, "contenido"))
-    
-    if not contenido:
-        contenido = (driver.find_elements(By.CLASS_NAME, "paragraph"))
 
-    # construcción listado con los elementos encontrados
-    articulo.append(titulo)
-    articulo.append(subtitulo)
-    for itemContenido in contenido:
-        textoContenido = itemContenido.text.strip()
-        if not textoContenido.startswith('(') and not textoContenido.endswith(')'):
-            articulo.append(itemContenido.text)
+    articuloContenido = (driver.find_elements(By.CLASS_NAME, "articulo-contenido"))
     
-    # exportar datos a .csv
-    df = pd.DataFrame({'texto':articulo})
-    df.to_csv('articulos_x_procesar/%s_%s_%i.csv' % (origen, snap, count), index=False)
-    count += 1
+    if articuloContenido:
+        contenido = articuloContenido[0].find_elements(By.CLASS_NAME, "contenido")
+    else :
+        contenido = (driver.find_elements(By.CLASS_NAME, "paragraph"))
+        
+    if  (titulo != '' and titulo != 'NONE') and subtitulo != "Hrm.":
+        # construcción listado con los elementos encontrados
+        articulo.append(titulo)
+        articulo.append(subtitulo)
+            
+        for itemContenido in contenido:
+            textoXlineas = itemContenido.text.strip().split('\n')
+            for texto in textoXlineas:
+                if texto and not texto.startswith('-') and (not texto.startswith('(') and not texto.endswith(')')):
+                    articulo.append(texto)
+        
+        # exportar datos a .csv
+        df = pd.DataFrame({linkArticulo:articulo})
+        df.to_csv('articulos_x_procesar/%s_%s_%i.csv' % (origen, snap, count), index=False)
+        count += 1
+    else :
+        articuloError.append(linkArticulo)
     
 driver.quit()
+
+if articuloError :
+    df3 = pd.DataFrame({'':articuloError})
+    df3.to_csv('log_ejecuciones/articulosError_2017_2024.csv', index=False)
